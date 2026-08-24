34266fc7efe51fe3fe2741d8746a2e6765940584
tarazonajerssondaniel <tarazonajerssondaniel@gmail.com>
2026-08-24 15:12:50 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pollosysabor.pos',
+  appName: 'Pollos & Sabor POS',
+  webDir: 'dist'
+};
+
+export default config;
